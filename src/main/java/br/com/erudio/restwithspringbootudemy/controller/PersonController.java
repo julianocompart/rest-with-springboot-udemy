22ba107e96222bffd4564d9af14be70b6c884cdf
julianocompart <juliano.csn@gmail.com>
2021-10-15 14:43:20 -0300

    Alterando PersonController - aula 68

--- src/main/java/br/com/erudio/restwithspringbootudemy/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringbootudemy/controller/PersonController.java
@@ -3,9 +3,14 @@ package br.com.erudio.restwithspringbootudemy.controller;
 import br.com.erudio.restwithspringbootudemy.exception.UnsupportedMathOperationException;
 import br.com.erudio.restwithspringbootudemy.model.Person;
 import br.com.erudio.restwithspringbootudemy.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +20,27 @@ import java.awt.*;
 @RequestMapping("/person")
 public class PersonController {
 
+    @Autowired
     private PersonServices service;
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person request){
+        return service.create(request);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person request){
+        return service.create(request);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
     }
 
 }
--- src/main/java/br/com/erudio/restwithspringbootudemy/services/PersonServices.java
+++ src/main/java/br/com/erudio/restwithspringbootudemy/services/PersonServices.java
@@ -1,8 +1,6 @@
 package br.com.erudio.restwithspringbootudemy.services;
 
 import br.com.erudio.restwithspringbootudemy.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,7 +9,21 @@ import java.util.concurrent.atomic.AtomicLong;
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
-    public Person findById(String id){
+    public Person findById(String id) {
         return new Person(counter.incrementAndGet(), "Juliano", "Silva", "Marcílio Noronha, Viana, ES, BR", "male");
+    }
+
+    public Person create(Person person) {
+        //entra aqui a logica que salva no banco de dados
+        return person;
+    }
+
+    public Person update(Person person) {
+        //entra aqui a logica que salva no banco de dados
+        return person;
+    }
+
+    public void delete(String id){
+        //logica para deletar, consultando o banco
     }
 }
